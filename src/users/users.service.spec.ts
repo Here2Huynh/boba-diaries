47798c859564b3a816b122eb02298cd318a1cf7c
John Huynh <jhnhnyh9@gmail.com>
2021-01-06 00:06:24 -0800

    splitted user methods into test cases, saving progress

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { UsersService } from './users.service';
+import { UserRepository } from './users.repository';
+import { User } from './user.entity';
+
+const mockUserRepository = () => ({
+  findOne: jest.fn(),
+});
+
+const mockUserService = () => ({});
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let userRepository;
+  let userService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: UserRepository, useFactory: mockUserRepository },
+      ],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    userRepository = module.get<UserRepository>(UserRepository);
+    userService = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('signUp', () => {
+    it('checks if user exists, if so, returns Conflict error', async () => {
+      //
+    });
+
+    it('if user does not exist, create user', async () => {
+      //
+    });
+  });
+
+  describe('validateUser', () => {
+    it('returns user if user is valid', async () => {
+      const testUser = new User();
+      testUser.username = 'testUsername';
+
+      userRepository.findOne.mockResolvedValue(testUser);
+
+      const res = await userService.validateUser({
+        username: 'testUsername',
+        password: 'testPassword',
+      });
+
+      expect(userRepository.findOne).toHaveBeenLastCalledWith({
+        username: 'testUsername',
+      });
+
+      expect(res).toEqual(testUser);
+    });
   });
 });
